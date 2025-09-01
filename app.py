af054094f776446b97b82b77b69221efb99d3311
bipzilla <infosandtechnos@gmail.com>
2025-09-01 19:23:19 +0100

    magic world

--- app.py
+++ app.py
@@ -12,15 +12,26 @@ sheet = gspread.service_account_from_dict(st.secrets["gcp_service_account"]) \
 # Form to add an expense
 who = st.text_input("Who paid?")
 desc = st.text_input("Description")
-amount = st.number_input("Amount (£)", min_value=0.0, step=0.5, format="%.2f")  # shows 5.00, 10.00 etc.
+amount = st.number_input("Amount (£)", min_value=0.0, step=0.5, format="%.2f") 
 
 if st.button("Add Expense"):
     if who and desc and amount > 0:
         ts = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        sheet.append_row([ts, who, desc, float(amount)])  # saves as a number in Google Sheets
-        st.success(f"✅ Added: {who} paid £{amount:.2f} for {desc}")
+        sheet.append_row([ts, who, desc, float(amount)])  
+        st.success(f"Added: {who} paid £{amount:.2f} for {desc}")
     else:
         st.warning("Please fill all fields correctly.")
 
 st.title("shows costs")
 
+st.write("this will list the indivisual costs")
+
+sheet = gspread.service_account_from_dict(st.secrets["gcp_service_account"]) \
+                  .open_by_key(st.secrets["sheet_id"]) \
+                  .worksheet("flatmates")
+
+
+flatmates = sheet.col_values(1)[1:]
+
+for x in flatmates:
+    st.write(x)
